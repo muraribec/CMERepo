54a72155e1a637f6fb633353a3531c5eef0912e1
unknown <murari>
2022-08-24 07:39:08 +0530

    created dealspage

---
+++ src/main/java/com/qa/hubspot/pages/DealPage.java
@@ -0,0 +1,11 @@
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.base.BasePage;
+
+public class DealPage extends BasePage{
+
+	
+	public void deal() {
+		System.out.println("This is a deal Page");
+	}
+}
